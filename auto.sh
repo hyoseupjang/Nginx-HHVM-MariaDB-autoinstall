779ae8e0a0e75d73c0be4966e0124916c6bfeaf2
nadanomics <hb7701@naver.com>
2015-09-14 14:01:22 +0900

    Update auto.sh
    remove auto firewall setting.

--- auto.sh
+++ auto.sh
@@ -63,33 +63,11 @@ wget https://raw.githubusercontent.com/nadanomics/Nginx-HHVM-MariaDB-autoinstall
 cd /usr/local/nginx/html
 echo ######################### Download hhvminfo.php #########################
 wget https://gist.githubusercontent.com/ck-on/67ca91f0310a695ceb65/raw/c0d9a376680ba5dc83e8f10475293f4042bda8a7/hhvminfo.php
-systemctl stop firewalld
-systemctl mask firewalld
-yum -y install iptables-services
-systemctl enable iptables
-rm -f /etc/sysconfig/iptables
-cat > /etc/sysconfig/iptables <<END
-# you can edit this manually or use system-config-firewall
-*filter
-:INPUT ACCEPT [0:0]
-:FORWARD ACCEPT [0:0]
-:OUTPUT ACCEPT [0:0]
--A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
--A INPUT -p icmp -j ACCEPT
--A INPUT -i lo -j ACCEPT
--A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
--A INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
--A INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT
--A INPUT -j REJECT --reject-with icmp-host-prohibited
--A FORWARD -j REJECT --reject-with icmp-host-prohibited
-COMMIT
-END
-systemctl restart iptables
 echo You need to edit nginx.conf worker_processes as your server core number. And do "nginx -t reload" to reload conf file. 
 echo You also need to do DB secure installation. 
 nginx
 mysql_secure_installation
 clear
-echo You can test webserver by accessing http://server_ip.  
+echo You can test webserver by accessing http://server_ip.(You need to open 80 port by using firewalld or iptables)
 echo you can get more information about this server, please watch hhvm info by accessing http://server_ip/hhvminfo.php
 echo And, you must remove hhvminfo.php after read about it. 
